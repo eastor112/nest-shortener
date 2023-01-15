2cc8b283e0040d78a09baa260bb31fa85ceaf644
emerson asto <emerar.mct@gmail.com>
2023-01-15 11:33:56 -0500

    Created scaffolding of shortener project

---
+++ src/url/entities/url.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Url {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  urlCode: string;
+
+  @Column()
+  longUrl: string;
+
+  @Column()
+  shortUrl: string;
+
+  @Column()
+  description: string;
+}
